424aab33137e1f9142b1983174bd22e26f9b3f8a
Alvaro Chao Ecija <alvarochaoecija@gmail.com>
2022-10-25 00:30:14 +0200

    Updated experimental code

--- R/Instantaneous and Lagged Coherence.R
+++ R/Instantaneous and Lagged Coherence.R
@@ -5,11 +5,16 @@
 
 
 
-CalculateCausalCouplings <- function(tf){
+CalculateCausalCouplings <- function(tf, use.thr = TRUE, thr = 0.5){
   Cospectrum <- tf$Cospectrum
   Quadrature <- tf$Quadrature
   Instantaneous <- abs(Cospectrum)^2
   Lagged <- (abs(Quadrature)^2) / (1 - Instantaneous)
+  if(used.thr){
+    Coherence <- tf$Coherence
+    Lagged[which(Coherence < thr)] <- NA
+    Instantaneous[which(Coherence < thr)] <- NA
+  }
   output <- list(Instantaneous = Instantaneous, Lagged = Lagged)
   return(output)
 }
