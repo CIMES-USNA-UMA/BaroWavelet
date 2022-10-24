77995ee3cdcf167b552e9f93d2d93741fbe35989
Alvaro Chao Ecija <alvarochaoecija@gmail.com>
2022-10-24 17:03:39 +0200

    Changed simulation parameters x2

--- R/Data Simulator from De Boer et Al.R
+++ R/Data Simulator from De Boer et Al.R
@@ -11,7 +11,7 @@ DataSimulation <- function(use.noise = TRUE){
   SBP <- 5  *sin(2*pi*0.1*(1:2000)) + 5  *sin(2*pi*0.25*(1:2000)) +
     use.noise * rnorm(2000, mean = 0, sd = 2)
   RR <- c(rep(9,1499), rep(3, 300), rep(9, 201)) * SBP
-  for(n in 7:2000) RR[n] <- RR[n] + sum(SBP[(n-2):(n-6)] * c(0.75,2,2.75,1.5,0.25))
+  for(n in 7:2000) RR[n] <- RR[n] + sum(SBP[(n-2):(n-6)] * c(1.5,2.75,2,0.75,0.25))
   set.seed(2)
   RR <- RR + use.noise * rnorm(2000, mean = 0, sd = 5)
   SBP <- SBP + 120
