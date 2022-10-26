e44ad7085765e351f63b7db926292bcefc778da6
Alvaro Chao Ecija <alvarochaoecija@gmail.com>
2022-10-26 18:39:07 +0200

    Improved phase restriction code

--- R/CWT TransferFun.R
+++ R/CWT TransferFun.R
@@ -48,13 +48,14 @@ TransferFunCWT <- function(data, HF = 0.4, LF = 0.15, VLF = 0.04,
                   XWT <- Smoothed$XWT
                   sXWT <- Smoothed$sXWT
                   if(phase.restrict){
-                    sXWT <- RestrictByPhase(XWT, sXWT, WT.x, feedback = feedback)
+                    sXWT_pr <- RestrictByPhase(XWT, sXWT, WT.x, feedback = feedback)
+                  } else {
+                    sXWT_pr <- sXWT
                   }
-                  TransferFun <- sXWT / sWT.x
-                  Cospectrum <- Re(sXWT/ sqrt(sWT.x * sWT.y))
-                  Quadrature <- Im(sXWT/ sqrt(sWT.x * sWT.y))
-                  #Coherence <- abs(sXWT)^2 / (sWT.x * sWT.y)
-                  Coherence <- abs(Cospectrum + Quadrature*1i)^2
+                  TransferFun <- sXWT_pr/ sWT.x
+                  Cospectrum <- Re(sXWT_pr/ sqrt(sWT.x * sWT.y))
+                  Quadrature <- Im(sXWT_pr/ sqrt(sWT.x * sWT.y))
+                  Coherence <- abs(sXWT)^2 / (sWT.x * sWT.y)
                   return(list(TransferFun = TransferFun, Coherence = Coherence,
                       Freqs = 1/WT.x$period, Cone = WT.x$coi, Time = data[,1],
                          HF = HF, LF = LF, VLF = VLF, type = "TFun_cwt",
