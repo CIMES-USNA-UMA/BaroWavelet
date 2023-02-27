89ffe3dfea9945e832bf6a3c26abc5c3347f0faa
Alvaro Chao Ecija <alvarochaoecija@gmail.com>
2023-02-27 01:02:02 +0100

    Improve code

--- R/Test Individual Variables.R
+++ R/Test Individual Variables.R
@@ -37,12 +37,13 @@ TestIndBRS <- function(fun, time_flags1, time_flags2, thr = 0.5,
 TestBRSDWT <- function(fun, time_flags1, time_flags2){
   time_flags1 <- time_flags1 * 60
   time_flags2 <- time_flags2 * 60
-  select_time1 <- fun$Time[(fun$Time >= as.numeric(time_flags1[1])) &
-                              (fun$Time <= as.numeric(time_flags1[2]))]
-  select_time2 <- fun$Time[(fun$Time >= as.numeric(time_flags2[1])) &
-                             (fun$Time <= as.numeric(time_flags2[2]))]
-  select_time1 <- match(select_time1, fun$Time)
-  select_time2 <- match(select_time2, fun$Time)
+  # The following steps are done due to floating point precision issues
+  select_time1 <- match(min(abs(time_flags1[1]-fun$Time)), 
+                        abs(time_flags1[1]-fun$Time)):match(min(abs(time_flags1[2]-fun$Time)), 
+                                                            abs(time_flags1[2]-fun$Time))
+  select_time2 <- match(min(abs(time_flags2[1]-fun$Time)), 
+                        abs(time_flags2[1]-fun$Time)):match(min(abs(time_flags2[2]-fun$Time)), 
+                                                            abs(time_flags2[2]-fun$Time))
   if((sum(select_time1 %in% select_time2)) != 0){
     if(((min(select_time1) - min(select_time2)) < 0) & 
        ((max(select_time1) - max(select_time2)) < 0)){
@@ -80,12 +81,13 @@ TestBRSCWT <- function(fun, time_flags1, time_flags2, thr = 0.5, use.thr = TRUE)
   VLF <- fun$VLF
   time_flags1 <- time_flags1 * 60
   time_flags2 <- time_flags2 * 60
-  select_time1 <- fun$Time[(fun$Time >= as.numeric(time_flags1[1])) &
-                             (fun$Time <= as.numeric(time_flags1[2]))]
-  select_time2 <- fun$Time[(fun$Time >= as.numeric(time_flags2[1])) &
-                             (fun$Time <= as.numeric(time_flags2[2]))]
-  select_time1 <- match(select_time1, fun$Time)
-  select_time2 <- match(select_time2, fun$Time)
+  # The following steps are done due to floating point precision issues
+  select_time1 <- match(min(abs(time_flags1[1]-fun$Time)), 
+                        abs(time_flags1[1]-fun$Time)):match(min(abs(time_flags1[2]-fun$Time)), 
+                                                            abs(time_flags1[2]-fun$Time))
+  select_time2 <- match(min(abs(time_flags2[1]-fun$Time)), 
+                        abs(time_flags2[1]-fun$Time)):match(min(abs(time_flags2[2]-fun$Time)), 
+                                                            abs(time_flags2[2]-fun$Time))
   if((sum(select_time1 %in% select_time2)) != 0){
     if(((min(select_time1) - min(select_time2)) < 0) & 
        ((max(select_time1) - max(select_time2)) < 0)){
@@ -156,13 +158,14 @@ TestIndHRandBP <- function(data, time_flags1, time_flags2){
   data$HR <- 60000/data$RR
   #data <- list(Time = data[,"Time"], HR = 60000/data[,"RR"], SBP = data[,"SBP"])
   time_flags1 <- time_flags1 * 60
-  select_time1 <- data$Time[(data$Time >= as.numeric(time_flags1[1])) &
-                             (data$Time <= as.numeric(time_flags1[2]))]
-  select_time1 <- match(select_time1, data$Time)
   time_flags2 <- time_flags2 * 60
-  select_time2 <- data$Time[(data$Time >= as.numeric(time_flags2[1])) &
-                             (data$Time <= as.numeric(time_flags2[2]))]
-  select_time2 <- match(select_time2, data$Time)
+  # The following steps are done due to floating point precision issues
+  select_time1 <- match(min(abs(time_flags1[1]-fun$Time)), 
+                        abs(time_flags1[1]-fun$Time)):match(min(abs(time_flags1[2]-fun$Time)), 
+                                                            abs(time_flags1[2]-fun$Time))
+  select_time2 <- match(min(abs(time_flags2[1]-fun$Time)), 
+                        abs(time_flags2[1]-fun$Time)):match(min(abs(time_flags2[2]-fun$Time)), 
+                                                            abs(time_flags2[2]-fun$Time))
   if((sum(select_time1 %in% select_time2)) != 0){
     if(((min(select_time1) - min(select_time2)) < 0) & 
        ((max(select_time1) - max(select_time2)) < 0)){
@@ -212,13 +215,14 @@ TestIndHRandBP <- function(data, time_flags1, time_flags2){
 #' TestIndHRV(AlphaIndex, c(0, 1.7), c(8, 9.5))
 TestIndHRV <- function(fun, time_flags1, time_flags2){
   time_flags1 <- time_flags1 * 60
-  select_time1 <- fun$Time[(fun$Time >= as.numeric(time_flags1[1])) &
-                             (fun$Time <= as.numeric(time_flags1[2]))]
-  select_time1 <- match(select_time1, fun$Time)
   time_flags2 <- time_flags2 * 60
-  select_time2 <- fun$Time[(fun$Time >= as.numeric(time_flags2[1])) &
-                             (fun$Time <= as.numeric(time_flags2[2]))]
-  select_time2 <- match(select_time2, fun$Time)
+  # The following steps are done due to floating point precision issues
+  select_time1 <- match(min(abs(time_flags1[1]-fun$Time)), 
+                        abs(time_flags1[1]-fun$Time)):match(min(abs(time_flags1[2]-fun$Time)), 
+                                                            abs(time_flags1[2]-fun$Time))
+  select_time2 <- match(min(abs(time_flags2[1]-fun$Time)), 
+                        abs(time_flags2[1]-fun$Time)):match(min(abs(time_flags2[2]-fun$Time)), 
+                                                            abs(time_flags2[2]-fun$Time))
   if((sum(select_time1 %in% select_time2)) != 0){
     if(((min(select_time1) - min(select_time2)) < 0) & 
        ((max(select_time1) - max(select_time2)) < 0)){
