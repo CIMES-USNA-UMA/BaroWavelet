60b511cbe18dab90ebe96ea0ab3e69bc29f9e117
Alvaro Chao Ecija <alvarochaoecija@gmail.com>
2023-08-24 19:41:02 +0200

    Add tag

--- R/Data_Simulator_from_De_Boer_Karemaker.R
+++ R/Data_Simulator_from_De_Boer_Karemaker.R
@@ -40,6 +40,7 @@ DataSimulation <-
       function(f,
                v,
                s) {
+        # For future feature
         lags <- 0:(NROW(c(v,s)) - 1)
         angle <- -lags * f * 360 
         phase <- angle * pi / 180
